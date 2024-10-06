8869dbb05e4ee91320e27fa3510f5defe605571e
Bhola Yadav <153095725+Bhya23cse@users.noreply.github.com>
2024-10-06 15:34:57 +0530

    Add files via upload

---
+++ cm.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  // cout << "Enter the number for thr numer";
+  int count = 4;
+  int p=1;
+
+  //char ch = 'A';
+  for (int i = 0; i < count+1; i++)
+  {
+    for (int j = 0; j < i; j++)
+    {
+      
+      cout << i;
+      //p++;
+    }
+    cout<<endl;
+  }
+
+  return 0;
+}
--- pattern.cpp
+++ pattern.cpp
@@ -1,24 +1,24 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the value of n: "<<endl;
+    cin>>n;
+   
+    for (int i = 0; i < n; i++)
+    { int ch=1;
+        for (int j = 0; j < i; j++)
+        {
+            cout<<ch<<" ";
+            ch++;
 
-int main()
-{
- 
-  int count = 4;
-  cout<<"Enter a number of row to print in capital letter: "<<endl;
-  cin>>count;
-  char ch='A';
-
-  for (int i = 0; i < count; i++)
-  {
-    for (int j = 0; j < count; j++)
-    {
-      
-      cout << ch<<" ";
-      ch++;
+            
+        }
+        cout<<endl;
+        
     }
-    cout<<endl<<endl;
-  }
+    cout<<endl;
+    
 
-  return 0;
+    return 0;
 }
